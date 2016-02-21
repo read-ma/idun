3e9c4bf93ffc1de3bf1204aa2e6be08c83fb3ab5
dariusz gorzeba <dgorzeba@gmail.com>
2016-02-21 01:02:17 +0100

    update production config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,7 +6,7 @@ var config = {
     entry: path.resolve(__dirname, './src/app/app.js'),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'public/bundle.js'
+        filename: 'bundle.js'
     },
     externals: {
         'Config': JSON.stringify({ apiUrl: 'http://ms-dashboard.herokuapp.com/api' })
@@ -36,7 +36,7 @@ var config = {
         ]
     },
     plugins: [
-        new ExtractTextPlugin('public/style.css', {
+        new ExtractTextPlugin('style.css', {
             allChunks: true
         })
     ]
