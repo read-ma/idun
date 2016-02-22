dba00d4c798649c35033f7da9c185ba564b47e29
Artur <janicki.artur@gmail.com>
2016-02-22 20:12:50 +0100

    fix mobile sidebar and prepare component

---
+++ src/app/components/MobileSidebar.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class MobileSidebar {
+
+    initSidebar() {
+        console.log('initSidebar');
+    }
+
+}
--- src/app/containers/Sidebar.js
+++ src/app/containers/Sidebar.js
@@ -4,7 +4,7 @@ import request from 'superagent';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { textSelected, toggleHighlighting } from '../actions';
-import { TTSPlayer, Wordlists, DefinitionBoxes } from '../components';
+import { TTSPlayer, Wordlists, DefinitionBoxes, MobileSidebar } from '../components';
 import UserCustomDefinitionForm from './UserCustomDefinition';
 import 'lodash';
 
@@ -14,7 +14,7 @@ class Sidebar extends Component {
         this.state = {
             selectedText: props.selectedText
         };
-
+        // this.MobileSidebar = new MobileSidebar();
         this.handleWordListSelected = this.handleWordListSelected.bind(this);
     };
 
@@ -23,6 +23,90 @@ class Sidebar extends Component {
             toggleHighlighting(event.target.name)
         );
     }
+
+    initMobileSidebar() {
+        var $ = document.querySelector.bind(document),
+        $$ = document.querySelectorAll.bind(document),
+        getPointerEvent = function(event) {
+            return event.targetTouches ? event.targetTouches[0] : event;
+        },
+
+        setListener = function (elm,events,callback) {
+            var eventsArray = events.split(' '),
+                i = eventsArray.length;
+            while(i--){
+                if(elm) {
+                    elm.addEventListener( eventsArray[i], callback, false );
+                }
+            }
+        };
+
+        var $touchArea = $('.mobile-sidebar-link'),
+            $wrapper = $('.wrapper'),
+            $sidebar = $('.sidebar'),
+            touchStarted = false, // detect if a touch event is sarted
+            currX = 0,
+            currY = 0,
+            cachedX = 0,
+            cachedY = 0;
+
+        //setting the events listeners
+        setListener($touchArea,'touchstart mousedown',function (e){
+            e.preventDefault();
+            var pointer = getPointerEvent(e);
+            // caching the current x
+            cachedX = currX = pointer.pageX;
+            // caching the current y
+            cachedY = currY = pointer.pageY;
+            // a touch event is detected
+            touchStarted = true;
+            console.log('Touch started');
+
+            // detecting if after 200ms the finger is still in the same position
+            setTimeout(function (){
+                if ((cachedX === currX) && !touchStarted && (cachedY === currY)) {
+                    // Here you get the Tap event
+                    // console.log("Tap");
+                }
+            },200);
+        });
+        setListener($touchArea,'touchend mouseup touchcancel',function (e){
+            e.preventDefault();
+            // here we can consider finished the touch event
+            touchStarted = false;
+            // console.log('Touch ended');
+            $sidebar.style.height = '250px';
+            $wrapper.style.height = '300px';
+        });
+        setListener($touchArea,'touchmove mousemove',function (e){
+            e.preventDefault();
+            var pointer = getPointerEvent(e);
+            currX = pointer.pageX;
+            currY = pointer.pageY;
+            // console.log('cahed: ');
+            // console.log(cachedY);
+            // console.log('curr: ');
+            // console.log(currY);
+            var h = cachedY - currY;
+            if(touchStarted && (currY > 250) && h > 0) {
+                 // here you are swiping
+                //  console.log(h);
+                 $wrapper.style.height = h + 50 + 'px';
+                 $sidebar.style.height = h + 'px';
+                //  console.log("Swiping");
+            }
+        });
+    };
+
+    componentDidUpdate() {
+        this.initMobileSidebar();
+    };
+
+    componentDidMount() {
+        window.addEventListener('load', function(){
+            this.initMobileSidebar();
+        });
+    };
 
     render() {
         return (
@@ -46,82 +130,6 @@ class Sidebar extends Component {
         );
     }
 }
-
-$(document).ready(function(){
-    var $ = document.querySelector.bind(document),
-    $$ = document.querySelectorAll.bind(document),
-    getPointerEvent = function(event) {
-        return event.targetTouches ? event.targetTouches[0] : event;
-    },
-
-    setListener = function (elm,events,callback) {
-        var eventsArray = events.split(' '),
-            i = eventsArray.length;
-        while(i--){
-            if(elm) {
-                elm.addEventListener( eventsArray[i], callback, false );
-            }
-        }
-    };
-
-    var $touchArea = $('.wrapper'),
-        $sidebar = $('.sidebar'),
-        touchStarted = false, // detect if a touch event is sarted
-        currX = 0,
-        currY = 0,
-        cachedX = 0,
-        cachedY = 0;
-
-    //setting the events listeners
-    setListener($touchArea,'touchstart mousedown',function (e){
-        e.preventDefault();
-        var pointer = getPointerEvent(e);
-        // caching the current x
-        cachedX = currX = pointer.pageX;
-        // caching the current y
-        cachedY = currY = pointer.pageY;
-        // a touch event is detected
-        touchStarted = true;
-        // console.log('Touch started');
-
-        // detecting if after 200ms the finger is still in the same position
-        setTimeout(function (){
-            if ((cachedX === currX) && !touchStarted && (cachedY === currY)) {
-                // Here you get the Tap event
-                // console.log("Tap");
-            }
-        },200);
-    });
-    setListener($touchArea,'touchend mouseup touchcancel',function (e){
-        e.preventDefault();
-        // here we can consider finished the touch event
-        touchStarted = false;
-        // console.log('Touch ended');
-        $sidebar.style.height = '250px';
-        $touchArea.style.height = '300px';
-    });
-    setListener($touchArea,'touchmove mousemove',function (e){
-        e.preventDefault();
-        var pointer = getPointerEvent(e);
-        currX = pointer.pageX;
-        currY = pointer.pageY;
-        // console.log('cahed: ');
-        // console.log(cachedY);
-        // console.log('curr: ');
-        // console.log(currY);
-        var h = cachedY - currY;
-        if(touchStarted && (currY > 250) && h > 0) {
-             // here you are swiping
-            //  console.log(h);
-             $touchArea.style.height = h + 50 + 'px';
-             $sidebar.style.height = h + 'px';
-            //  console.log("Swiping");
-        }
-        // if(touchStarted && (currY > 250) && h > 0) {
-        //
-        // }
-    });
-});
 
 function mapStateToProps(state){
     return {
