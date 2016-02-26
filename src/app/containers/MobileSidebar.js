e7e09c7d3f1dfdfffdffb0061d04b9c313b0e7c5
Artur <janicki.artur@gmail.com>
2016-02-26 18:43:52 +0100

    fix MobileSidebar

--- src/app/containers/MobileSidebar.js
+++ src/app/containers/MobileSidebar.js
@@ -2,6 +2,7 @@ var MobileSidebarMixin = {
     initMobileSidebar: function() {
         var $ = document.querySelector.bind(document),
             $$ = document.querySelectorAll.bind(document),
+
             getPointerEvent = function(event) {
                 return event.targetTouches ? event.targetTouches[0] : event;
             },
@@ -19,59 +20,57 @@ var MobileSidebarMixin = {
         var $touchArea = $('.mobile-sidebar-link'),
             $wrapper = $('.wrapper'),
             $sidebar = $('.sidebar'),
-            touchStarted = false, // detect if a touch event is sarted
+            touchStarted = false,
+            firstTouch = true,
             pageY = 0,
             currY = 0,
             cachedY = 0;
 
-        //setting the events listeners
-        setListener($touchArea,'touchstart mousedown',function (e){
+        setListener($touchArea,'touchstart',function (e){
             e.preventDefault();
             var pointer = getPointerEvent(e);
-
-            cachedY = pageY = pointer.pageY;
+            cachedY = pointer.pageY;
+            currY = parseInt($sidebar.style.height) || 0;
             touchStarted = true;
             console.log('Touch started');
         });
-        setListener($touchArea,'touchend mouseup touchcancel',function (e){
+
+        setListener($touchArea,'touchend touchcancel',function (e){
             e.preventDefault();
-            // here we can consider finished the touch event
             touchStarted = false;
+            firstTouch = false;
             console.log('Touch ended');
         });
-        setListener($touchArea,'touchmove mousemove',function (e){
+
+        setListener($touchArea,'touchmove',function (e){
             e.preventDefault();
-            var pointer = getPointerEvent(e);
+            var pointer = getPointerEvent(e),
+                wrapperHeight = parseInt($wrapper.style.height) || 50;
+
             pageY = pointer.pageY;
 
-            if(!currY) {
-                currY = 0;
+            if(touchStarted && (pageY < cachedY) && (wrapperHeight >= 50))  {
+                if(firstTouch){
+                    var newHeight = cachedY - pageY;
+                    $wrapper.style.height = newHeight + 50 + 'px';
+                    $sidebar.style.height = newHeight + 'px';
+                } else {
+                    var newHeight = currY + (cachedY - pageY);
+                    $wrapper.style.height = newHeight + 50 + 'px';
+                    $sidebar.style.height = newHeight + 'px';
+                }
             }
 
-            console.log('cahed: ');
-            console.log(cachedY);
-            console.log('curr: ');
-            console.log(currY);
-            console.log('pageY:');
-            console.log(pageY);
-            console.log('------------------');
-
-            if(touchStarted && (pageY < cachedY) && (currY >= 0)) {
-                // console.log('wchodzimy');
-                $wrapper.style.height = (cachedY - pageY) + 'px';
-                $sidebar.style.height = (cachedY - pageY) + 'px';
-                currY = currY + (cachedY - pageY);
-                console.log('curr: ');
-                console.log(currY);
-                console.log('--------ddd------');
+            if(touchStarted && (pageY > cachedY) && (wrapperHeight >= 50) && !firstTouch) {
+                var newHeight = currY - (pageY - cachedY);
+                if(newHeight >= 50) {
+                    $wrapper.style.height = currY - (pageY - cachedY) + 'px';
+                    $sidebar.style.height = currY - (pageY - cachedY) - 50 + 'px';
+                } else {
+                    $wrapper.style.height = 50 + 'px';
+                    $sidebar.style.height = 0 + 'px';
+                }
             }
-            if(touchStarted && (pageY > cachedY) && (currY >= 0)) {
-                // console.log('schodzimy');
-                $wrapper.style.height = (pageY - cachedY) + 'px';
-                $sidebar.style.height = (pageY - cachedY) + 'px';
-                currY = currY - (pageY - cachedY);
-            }
-
         });
 
     }
