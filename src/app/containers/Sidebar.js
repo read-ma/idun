ba7f5212868f2e128027e0a95b360a7c4d7cfef5
dariusz gorzeba <dgorzeba@gmail.com>
2016-03-09 10:05:58 +0100

    reformat code

--- src/app/containers/Sidebar.js
+++ src/app/containers/Sidebar.js
@@ -6,129 +6,128 @@ import { connect } from 'react-redux';
 import { textSelected, toggleHighlighting, changeBoxOrder } from '../actions';
 import { Wordlists, DefinitionBoxes } from '../components';
 import UserCustomDefinitionForm from './UserCustomDefinition';
-import 'lodash';
+import _ from 'lodash';
 import classnames from 'classnames';
 import LanguageBar from '../components/LanguageSelection';
 
 const Sidebar = React.createClass({
 
-    getInitialState: function() {
-        return {
-            selectedText: this.props.selectedText
-        };
-    },
+  getInitialState: function() {
+    return {
+      selectedText: this.props.selectedText
+    };
+  },
 
-    handleWordListSelected: function(event){
-        this.props.dispatch(
-            toggleHighlighting(event.target.name)
-        );
-    },
+  handleWordListSelected: function(event){
+    this.props.dispatch(
+      toggleHighlighting(event.target.name)
+    );
+  },
 
-    handlePicturesBoxOrderUpdated: function(event){
-        this.props.dispatch(
-            changeBoxOrder('graphics', event.target.value)
-        );
-    },
+  handlePicturesBoxOrderUpdated: function(event){
+    this.props.dispatch(
+      changeBoxOrder('graphics', event.target.value)
+    );
+  },
 
-    toggleSettingsPanelVisible: function(event) {
-        this.setState(
-            Object.assign({}, this.state, {settingVisible: !this.state.settingVisible})
-        );
-    },
+  toggleSettingsPanelVisible: function(event) {
+    this.setState(
+      Object.assign({}, this.state, {settingVisible: !this.state.settingVisible})
+    );
+  },
 
-    render: function() {
-        return (
-            <div className="wrapper">
-              <div className="hide-on-large-only">
-                <div className="">Show Sidebar</div>
-              </div>
-              <aside className='sidebar'>
-                <div className="row">
-                  <a className="right btn-floating" onClick={this.toggleSettingsPanelVisible}><i className="material-icons">settings</i></a>
-                </div>
+  render: function() {
+    return (
+      <div className="wrapper">
+        <div className="hide-on-large-only">
+          <div className="">Show Sidebar</div>
+        </div>
+        <aside className='sidebar'>
+          <div className="row">
+            <a className="right btn-floating" onClick={this.toggleSettingsPanelVisible}><i className="material-icons">settings</i></a>
+          </div>
 
-                <ul>
-                  <li className={classnames('card',{hidden: !this.state.settingVisible})} ref="settingsPanel" >
-                    <BoxesOrder header='Pictures box position' handleUpdate={this.handlePicturesBoxOrderUpdated} />
-                    <Wordlists handleSelected={this.handleWordListSelected} wordlists={this.props.wordlists} header="Highlighting"/>
-                    <LanguageBar />
-                  </li>
+          <ul>
+            <li className={classnames('card',{hidden: !this.state.settingVisible})} ref="settingsPanel" >
+              <BoxesOrder header='Pictures box position' handleUpdate={this.handlePicturesBoxOrderUpdated} />
+              <Wordlists handleSelected={this.handleWordListSelected} wordlists={this.props.wordlists} header="Highlighting"/>
+              <LanguageBar />
+            </li>
 
-                  <li><UserCustomDefinitionForm /></li>
+            <li><UserCustomDefinitionForm /></li>
 
-                  <li className="card">
-                    <UserCustomDefinition userDefinitions={this.props.userDefinitions} selectedText={this.props.selectedText} />
-                  </li>
+            <li className="card">
+              <UserCustomDefinition userDefinitions={this.props.userDefinitions} selectedText={this.props.selectedText} />
+            </li>
 
-                  <li><DefinitionBoxes /></li>
+            <li><DefinitionBoxes /></li>
 
-                </ul>
-              </aside>
-            </div>
-        );
-    }
+          </ul>
+        </aside>
+      </div>
+    );
+  }
 });
 
 class UserCustomDefinition extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {userDefinition: {}};
-    }
+  constructor(props){
+    super(props);
+    this.state = {userDefinitions: []};
+  }
 
-    componentWillReceiveProps(nextProps){
-        let userDefinition = _.find(nextProps.userDefinitions, {word: nextProps.selectedText});
-        if (userDefinition)
-            this.setState({userDefinition:  userDefinition});
-        else
-            this.setState({userDefinition:  {}});
-    }
+  componentWillReceiveProps(nextProps){
+    let definitions = nextProps.userDefinitions.filter(def => def.word == nextProps.selectedText);
 
-    render() {
-        if (this.state.userDefinition.translation)
-            return (
-                <ul className="collection with-header">
-                  <li className="collection-header">
-                    <h5>Your definition</h5>
-                  </li>
-                  <li className="collection-item" dangerouslySetInnerHTML={{__html: this.state.userDefinition.translation}}></li>
-                  <li className="collection-item" dangerouslySetInnerHTML={{__html: this.state.userDefinition.definition}}></li>
-                </ul>
-            );
-        else
-            return false;
+    this.setState({userDefinitions:  definitions});
+  }
+
+  render() {
+    if (this.state.userDefinitions.length > 0){
+      let definitions = this.state.userDefinitions.map(def => <li className="collection-item" dangerouslySetInnerHTML={{__html: def.translation}} />);
+      return (
+        <ul className="collection with-header">
+          <li className="collection-header">
+            <h5>Your definition</h5>
+          </li>
+          {definitions}
+        </ul>
+      );
+    }
+    else
+      return false;
     }
 }
 
 // todo move to setings
 class BoxesOrder extends Component {
-    render() {
+  render() {
 
-        return (
-            <ul className="collection">
-              <li className="collection-item">
-                <h5>{this.props.header}</h5>
-              </li>
-              <li className="collection-item">
-                <input name="picturesBoxPosition" type="radio" id="pictureFirst" value='first' onChange={this.props.handleUpdate} />
-                <label htmlFor="pictureFirst">First</label>
-              </li>
-              <li className="collection-item">
-                <input name="picturesBoxPosition" type="radio" id="pictureLast" value='last' onChange={this.props.handleUpdate} />
-                <label htmlFor="pictureLast">Last</label>
-              </li>
-            </ul>
-        );
+    return (
+      <ul className="collection">
+        <li className="collection-item">
+          <h5>{this.props.header}</h5>
+        </li>
+        <li className="collection-item">
+          <input name="picturesBoxPosition" type="radio" id="pictureFirst" value='first' onChange={this.props.handleUpdate} />
+          <label htmlFor="pictureFirst">First</label>
+        </li>
+        <li className="collection-item">
+          <input name="picturesBoxPosition" type="radio" id="pictureLast" value='last' onChange={this.props.handleUpdate} />
+          <label htmlFor="pictureLast">Last</label>
+        </li>
+      </ul>
+    );
 
-    }
+  }
 }
 
 function mapStateToProps(state){
-    return {
-        selectedText: state.article.selectedText,
-        wordlists: state.wordlists,
-        userDefinitions: state.main.userDefinitions
-    };
+  return {
+    selectedText: state.article.selectedText,
+    wordlists: state.wordlists,
+    userDefinitions: state.main.userDefinitions
+  };
 
 }
 
