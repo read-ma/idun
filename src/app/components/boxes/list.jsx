c4379c2bb90148f1f52bf3898091051b9769706d
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2016-03-13 15:43:08 +0100

    Sidebar: collapse results

--- src/app/components/boxes/list.jsx
+++ src/app/components/boxes/list.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import _ from 'lodash';
+import classnames from 'classnames';
 
 const LanguageIcon = ({lang}) => {
   let language = lang === 'en' ? 'gb' : lang;
@@ -36,14 +37,47 @@ function DefinitionListItem({text, language, url, partOfSpeech, handleClick, key
 }
 
 class DefinitionList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {collapsed: false};
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({collapsed: !this.state.collapsed})
+  }
+
   render() {
-    let items = this.props.items
-                    .map( item => DefinitionListItem(Object.assign({}, item, {key: _.uniqueId('definitionlist')}, {handleClick: this.props.handleClick})));
+    const itemsToShowNumber = 2;
+    let visibleItems = this.props.items.slice(0, itemsToShowNumber).map( (item) =>
+      DefinitionListItem(Object.assign({}, item, {key: _.uniqueId('definitionlist')}, {handleClick: this.props.handleClick}))
+    );
+
+    let hiddenItems = this.props.items.slice(itemsToShowNumber).map( (item) =>
+      DefinitionListItem(Object.assign({}, item, {key: _.uniqueId('definitionlist')}, {handleClick: this.props.handleClick}))
+    );
+
+    let collapseBox;
+    if (hiddenItems.length > 0) {
+      collapseBox = (
+        <div>
+          <li className="collection-header col m12 right-align">
+            <a  onClick={this.toggle}>
+              Show all
+            </a>
+          </li>
+          <div className={classnames({hide: !this.state.collapsed})}>
+            {hiddenItems}
+          </div>
+        </div>
+      )
+    }
 
     return (
       <ul className="collection with-header white">
         <li className="collection-header"><h5>{this.props.label}</h5></li>
-        {items}
+        {visibleItems}
+        {collapseBox}
       </ul>
     );
   }
