518e5ef7b742ddebd59d6c1a95ee3d83dfd0060f
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2016-03-13 11:52:11 +0100

    Cards: allow marking flashcards

--- src/app/components/UserDefinitions.jsx
+++ src/app/components/UserDefinitions.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { connect } from 'react-redux';
 import { loadUserDefinitions } from '../actions';
 
 function mapStateToProps(state) {
-  return {
-    items: state.main.userDefinitions
-  };
+  return { items: state.main.userDefinitions };
 }
 
-class UserDefinitionBox extends Component {
-  handleMouseOver(event) {
+class UserDefinitionFlashcard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markedInSession: false
+    };
+  }
+
+  markEasy() {
+    this.props.markItem(this.props.item, 'easy');
+    this.markViewed();
+  }
+
+  markHard() {
+    this.props.markItem(this.props.item, 'hard');
+    this.markViewed();
+  }
+
+  markViewed() {
+    this.setState({ markedInSession: true });
+  }
+
+  renderMarkButtons() {
+    if (true || !this.state.markedInSession) {
+      return (<div className="row">
+          <a className="btn green" onClick={this.markEasy.bind(this)}>Easy</a>
+          <a className="btn red" onClick={this.markHard.bind(this)}>Hard</a>
+        </div>
+      )
+    } else {
+      return (<div className="row"></div>)
+    }
   }
 
   render() {
@@ -18,7 +47,26 @@ class UserDefinitionBox extends Component {
         <div className='card-content'>
           <h5 className='card-title blue-text'>{this.props.item.word}</h5>
           <h6 className='card-subtitle'>{this.props.item.translation}</h6>
-          <div className='card-description'>{this.props.item.definition}</div>
+          <div className='card-description'>
+            {this.props.item.definition}
+          </div>
+          {this.renderMarkButtons()}
+        </div>
+      </div>
+    );
+  }
+}
+
+class UserDefinitionBox extends Component {
+  render() {
+    return (
+      <div className="userdefinitionbox card-item" key={this.props.item.id}>
+        <div className='card-content'>
+          <h5 className='card-title blue-text'>{this.props.item.word}</h5>
+          <h6 className='card-subtitle'>{this.props.item.translation}</h6>
+          <div className='card-description'>
+            {this.props.item.definition}
+          </div>
         </div>
       </div>
     );
@@ -28,12 +76,19 @@ class UserDefinitionBox extends Component {
 class UserDefinitions extends Component {
   constructor(props) {
     super(props);
-    this.state = { items: props.items };
+    this.state = {
+      items: props.items,
+      currentItem: props.items[0]
+    };
     this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.markItem = this.markItem.bind(this);
   }
 
-  componentWillReceiveProps(nextProps){
-    this.setState({ items: nextProps.items });
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      items: nextProps.items,
+      currentItem: nextProps.items[0]
+    });
   }
 
   handleFilterChange(event) {
@@ -50,19 +105,51 @@ class UserDefinitions extends Component {
     );
   }
 
-  render(){
-    let items =  this.state.items.map( item => <UserDefinitionBox item={item}/>);
+  markItem(item, markValue) {
+    // this.setState changeattribute for item and change all state?
 
+    this.setState({currentItem: this.fetchNextElement(this.state.items, this.state.currentItem)});
+  }
+
+  fetchNextElement(list, element) {
+    let nextElementIndex = list.indexOf(element) + 1;
+    if (nextElementIndex >= list.length) {
+      nextElementIndex = 0
+    }
+
+    return list[nextElementIndex]
+  }
+
+  renderFlashcard() {
+    let result;
+    if (this.state.currentItem) {
+      result = (
+        <ReactCSSTransitionGroup transitionName="fadein" transitionAppear={false} transitionLeave={false} transitionEnterTimeout={500} transitionLeaveTimeout={300}>
+          <div className="row" key={this.state.currentItem.word}>
+              <UserDefinitionFlashcard item={this.state.currentItem} markItem={this.markItem} />
+          </div>
+        </ReactCSSTransitionGroup>
+      )
+    } else {
+      result = (<div className="row"></div>);
+    }
+
+    return result;
+  }
+
+  render() {
+    const items = this.state.items.map(item => <UserDefinitionBox item={item} />);
 
     return (
-
       <div className="articles">
+        <div className="cards-container">
+          {this.renderFlashcard()}
+        </div>
         <input type='search' onChange={this.handleFilterChange} name='word' placeholder='quick search...'/>
         <div className="cards-container">
           {items}
         </div>
       </div>
-
     );
   }
 }
