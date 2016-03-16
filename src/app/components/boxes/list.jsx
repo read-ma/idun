994f6fa93c0bbf1938765544f3de30678000465c
Paweł Kowalski <pavelloz@gmail.com>
2016-03-16 12:28:06 +0100

    Sidebar enhancements

--- src/app/components/boxes/list.jsx
+++ src/app/components/boxes/list.jsx
@@ -15,6 +15,9 @@ const LanguageIcon = ({lang}) => {
 };
 
 function DefinitionListItem({text, language, url, partOfSpeech, handleClick, key}) {
+  function renderPartOfSpeech(partOfSpeech) {
+    return partOfSpeech ? <small>{partOfSpeech}</small> : false;
+  }
 
   function add(){
     handleClick({translation: text});
@@ -22,7 +25,7 @@ function DefinitionListItem({text, language, url, partOfSpeech, handleClick, key
 
   if (url)
     return (
-      <li className="collection-item col m6 white" key={key}>
+      <li className="collection-item item-image center-align col s12 m6" key={key}>
         <img data-caption={text} src={url} alt={text} />
       </li>
     );
@@ -31,7 +34,7 @@ function DefinitionListItem({text, language, url, partOfSpeech, handleClick, key
       <li className="collection-item" key={key}>
         <a className="secondary-content badge"><i className="material-icons" onClick={add}>add</i></a>
         <LanguageIcon lang={language} />
-        <small>{partOfSpeech}</small>
+        {renderPartOfSpeech(partOfSpeech)}
         <div dangerouslySetInnerHTML={{__html: text}}></div>
       </li>
     );
@@ -61,24 +64,23 @@ class DefinitionList extends Component {
     let collapseBox;
     if (hiddenItems.length > 0) {
       collapseBox = (
-        <div>
-          <li className="collection-header col m12 right-align">
-            <a  onClick={this.toggle}>
-              Show all
-            </a>
-          </li>
-          <div className={classnames({hide: !this.state.collapsed})}>
-            {hiddenItems}
-          </div>
+        <div className={classnames({hide: !this.state.collapsed})}>
+          {hiddenItems}
         </div>
       )
     }
 
     return (
       <ul className="collection with-header">
-        <li className="collection-header"><h5>{this.props.label}</h5></li>
-        {visibleItems}
+        <li className="collection-header">
+          <a className="right" onClick={this.toggle}>
+            {!this.state.collapsed ? 'Show more' : 'Show less'}
+          </a>
+          <h5>{this.props.label}</h5>
+        </li>
+
         {collapseBox}
+        {visibleItems}
       </ul>
     );
   }
