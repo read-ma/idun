47d2e69a1458977f26b371bb15f3d6f183502957
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2016-03-17 22:34:51 +0100

    Notify when definition added

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -39,6 +39,7 @@ function userDefinitionSavingFailed(error){
 }
 
 function userDefinitionSaved(definition){
+  Materialize.toast('Definition added', 4000);  
   return {
     type: 'USER_DEFINITION_SAVED',
     definition: definition
