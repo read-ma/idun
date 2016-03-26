5f009c48952777c3c95fe86428d20902d6fa55ac
dariusz gorzeba <dgorzeba@gmail.com>
2016-03-26 08:25:06 +0100

    update request building due new API

--- src/app/actions/definitions.js
+++ src/app/actions/definitions.js
@@ -15,10 +15,15 @@ const sanitizeText = text =>
                .replace(/\./g, ' '));
 
 function findWordData(text, type, options={}){
-  let params = Object.assign({}, options, {type: type}, Language.keysOfCurrent());
+  let params = Object.assign(
+    {},
+    options,
+    {query: sanitizeText(text)},
+    {type: type},
+    Language.keysOfCurrent());
 
   return (dispatch) => {
-    api.get(`/translate/${sanitizeText(text)}.json`, {params: params})
+    api.get(`/translate/${type}.json`, {params: params})
       .then( response => dispatch(contentLoaded(type, response.data)))
       .catch( error => {
         console.log(error);
