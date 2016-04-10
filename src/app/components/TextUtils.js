80201f27dda8d7b2c66feea1776e5f2bea47bb9a
dariusz gorzeba <dgorzeba@gmail.com>
2016-04-10 02:20:34 +0200

    better detokenizer

--- src/app/components/TextUtils.js
+++ src/app/components/TextUtils.js
@@ -12,17 +12,29 @@ class Token {
   }
 }
 
+const isQuoteMark = (token) => token.match(/^[\"\'\“\”]$/);
+
 const detokenize = tokens => {
   let output = [];
 
+  let openningQuoteMark = false;
+
   tokens.forEach(p => {
-    let spaceBefore = p.props.separator && p.props.word.match(/^[\(\”\”\"\'\“]$/);
+    let quote = p.props.separator && isQuoteMark(p.props.word);
+
+    if (quote) openningQuoteMark = !openningQuoteMark;
+
+    let spaceBefore = p.props.separator && p.props.word.match(/^[\(\“]$/);
     let previous    = output[output.length - 1];
-    let noSpace     = previous && previous.props.separator && previous.props.word.match(/^[\(\[]$/);
+    let noSpace     = previous && previous.props.separator && (previous.props.word.match(/^[\(\[]$/) || ( isQuoteMark(previous.props.word) && openningQuoteMark ));
 
     if (spaceBefore && !noSpace){
       output.push(' ');
     }
+
+    if (quote && openningQuoteMark)
+      output.push(' ');
+
 
     if (!p.props.separator && output.length != 0 && !noSpace)
       output.push(' ');
@@ -31,7 +43,7 @@ const detokenize = tokens => {
   });
 
   return output;
-}
+};
 
 const isSeparator = token => token.match(/^[\,\.\?\!\)\(\)\”\"\'\:\;\“]$/);
 const Separator = ( {children, separator} ) => <span>{children}</span>;
