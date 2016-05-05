a31014b7b5f4425c6cfdc5b7340d42f8ec4e0ab8
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2016-05-05 22:17:44 +0200

    Article: add article by pasting text

--- src/app/components/AddArticleWidget.jsx
+++ src/app/components/AddArticleWidget.jsx
@@ -7,6 +7,7 @@ export default class ArticleAdd extends React.Component {
     this.state = {inputValue: ''};
     this.addUrl = this.addUrl.bind(this);
     this.onChange = this.onChange.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
   addUrl(event) {
@@ -24,22 +25,74 @@ export default class ArticleAdd extends React.Component {
    this.setState({ inputValue: e.target.value });
   }
 
+  /* Workaround
+    Dirty hack to prevent BG overlapping modal
+  */
+  removeModalBackground() {
+    $('.lean-overlay').remove();
+  }
+
+  getModalSettings() {
+    return {
+      dismissible: true,
+      opacity: 0,
+      in_duration: 100,
+      out_duration: 100,
+      ready: this.removeModalBackground
+    };
+  }
+
+  toggleModal(event) {
+    event.preventDefault();
+
+    let $modal = $('#addArticleModal');
+
+    if (!this.state.settingVisible) {
+      $modal.openModal(this.getModalSettings());
+    } else {
+      $modal.closeModal(this.getModalSettings());
+    }
+
+    this.toggleVisibilityState();
+  }
+
+  toggleVisibilityState() {
+    this.setState(Object.assign({}, this.state, {settingVisible: !this.state.settingVisible}) );
+  }
+
   render () {
     return (
       <div className="row">
-        <form onSubmit={this.addUrl}>
-          <div className="col s10">
-            <div className="input-field">
-              <input type="url" name="sourceUrl" className="validate" ref="urlInput" id="sourceUrl" required="required" onChange={this.onChange} value={this.state.inputValue} />
-              <label htmlFor="sourceUrl">Add article from url</label>
+        <div id="addArticleModal" className="modal">
+          <form onSubmit={this.addUrl}>
+            <div className="modal-content">
+              <h4 className="left-align">Add article</h4>
+                <div className="input-field col s12">
+                  <textarea id="article-source-text" className="materialize-textarea"></textarea>
+                  <label htmlFor="article-source-text">Paste your text</label>
+                </div>
+
+                <div className="center-align row">
+                  Or
+                </div>
+
+                <div className="input-field">
+                  <input type="url" name="sourceUrl" className="validate" ref="urlInput" id="sourceUrl" required="required" onChange={this.onChange} value={this.state.inputValue} />
+                  <label htmlFor="sourceUrl">Enter URL for article to import</label>
+                </div>
+
             </div>
-          </div>
-          <div className="col s2">
-            <button type="submit" className="btn-floating btn-large green">
-              <i className="material-icons">add</i>
-            </button>
-          </div>
-        </form>
+            <div className="modal-footer">
+              <a href="#!" className=" modal-action modal-close waves-effect waves-green btn-flat">Add article</a>
+            </div>
+          </form>
+        </div>
+
+        <div className="col s2 offset-s10">
+          <button type="submit" className="btn-floating btn-large green" onClick={this.toggleModal}>
+            <i className="material-icons">add</i>
+          </button>
+        </div>
       </div>
     );
   }
