36f410a7c169d8ffe7fca68276fcf70611b52bd7
Paweł Kowalski <pavelloz@gmail.com>
2016-05-10 16:20:04 +0200

    Fix articles route

--- src/app/config/routes.jsx
+++ src/app/config/routes.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
-import {Home,Main, Articles, ArticlePage, UserDefinitionsLearn, UserDefinitionsList} from '../components';
+import { Home, Main, Articles, ArticlePage, UserDefinitionsLearn, UserDefinitionsList } from '../components';
 import Login from '../components/Login';
 import SignUpForm from '../components/SignUpForm';
 import Profile from '../components/Profile';
 import PasswordReminderView from '../components/PasswordReminderView';
 import ChangePasswordView from '../components/ChangePasswordView';
 
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, IndexRedirect } from 'react-router';
 import { requireAuthentication } from '../utils';
 
 import ArticleFilter from '../components/ArticleFilter';
 
 export default (
   <Route path="/" component={Main}>
-    <Route path="/login" component={Login}/>
-    <Route path="/sign_up" component={SignUpForm}/>
-    <Route path="forgot_password" component={PasswordReminderView}/>
-    <Route path="reset_password" component={ChangePasswordView}/>
+    <Route path="/login" component={Login} />
+    <Route path="/sign_up" component={SignUpForm} />
+    <Route path="forgot_password" component={PasswordReminderView} />
+    <Route path="reset_password" component={ChangePasswordView} />
 
     <Route path="/learn" component={requireAuthentication(Profile)}>
       <Route path="definitions" component={UserDefinitionsList} />
       <IndexRoute component={UserDefinitionsLearn} />
     </Route>
 
-    <Route path="/articles" components={{children: requireAuthentication(Articles), navChildren: ArticleFilter }} />
-    <Route path="/article/:id" component={requireAuthentication(ArticlePage)} displaySearchBar={true}/>
-    <IndexRoute component={requireAuthentication(Articles)} />
+    <Route path="/articles" components={{ children: requireAuthentication(Articles), navChildren: ArticleFilter }} />
+    <Route path="/article/:id" component={requireAuthentication(ArticlePage)} displaySearchBar={true} />
+    <IndexRedirect to="/articles" />
   </Route>
 
 );
