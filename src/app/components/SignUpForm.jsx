365267d320c9e0454210b387d0c8b5294413ff72
Paweł Kowalski <pavelloz@gmail.com>
2016-06-12 22:41:00 +0200

    Improve styling of sign up page

--- src/app/components/SignUpForm.jsx
+++ src/app/components/SignUpForm.jsx
@@ -1,6 +1,28 @@
+// TODO: Extract pre-login form styles.
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { signupAttempt } from '../actions/auth';
+
+import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+const styles = {
+  headline: {
+    fontSize: 18,
+    marginBottom: 20,
+    fontWeight: 500,
+    color: '#444'
+  },
+  sendButton: {
+    marginTop: '1em',
+    marginRight: '1em'
+  },
+  noSpamPromise: {
+    color: '#bbb',
+    fontSize: '11px'
+  }
+};
 
 class SignUpForm extends React.Component {
 
@@ -22,29 +44,29 @@ class SignUpForm extends React.Component {
 
   render() {
     return (
-      <div className="signupform">
-        <h2>Sign up</h2>
-        <p className="flow-text">
+      <form onSubmit={this.handleSignUp.bind(this)} className="col-xs-12 col-md-8 col-lg-6">
+        <h2 style={styles.headline}>Create account</h2>
+        <p style={{ marginBottom: '15px' }}>
           If you are interested in the beta testing of ReadMa, <br/>
-          let us know using the form below. <br/>
-          We will get back to you as soon as we can.
+          let us know using the form below.
         </p>
-        <form onSubmit={this.handleSignUp.bind(this)}>
-          <div className="input-field">
-            <input
-              name="email"
-              id="signUpEmail"
-              type="email"
-              required="true"
-              onChange={this.handleFormInputChanged.bind(this)}
-              className="validate" />
-            <label htmlFor="signUpEmail">Your email</label>
-          </div>
-          <h5 className="green-text">{this.props.signupMessage}</h5>
-          <input className="btn" type="submit" value="Sign up" />
-          <small className="right">We don't spam, promise.</small>
-        </form>
-      </div>
+        <p>We will get back to you as soon as we can.</p>
+
+        <h5 className="green-text">{this.props.signupMessage}</h5>
+
+        <TextField
+          floatingLabelText="Your email address"
+          type="email"
+          id="signUpEmail"
+          name="email"
+          required="true"
+          fullWidth={true}
+          onChange={this.handleFormInputChanged.bind(this)}
+        />
+
+        <RaisedButton label="Create account" primary={true} type="submit" style={styles.sendButton} />
+        <small style={styles.noSpamPromise}>We don't spam, promise.</small>
+      </form>
     );
   }
 }
