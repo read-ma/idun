0fdc4acd65eae37804f9e87c788a6d2041a0d9b3
Paweł Kowalski <pavelloz@gmail.com>
2016-06-12 22:34:12 +0200

    Improve styling of reset password page

--- src/app/components/ChangePasswordView.jsx
+++ src/app/components/ChangePasswordView.jsx
@@ -1,8 +1,25 @@
+// TODO: verify if materialui conversion went good. Did it without any preview... ;)
+
 import React from 'react';
 import { connect } from 'react-redux';
 import { updatePassword } from '../actions/auth';
 import NotificationBox from './NotificationBox';
 import { push } from 'react-router-redux';
+
+import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+const styles = {
+  headline: {
+    fontSize: 18,
+    marginBottom: 20,
+    fontWeight: 500,
+    color: '#444'
+  },
+  sendButton: {
+    marginTop: '1em'
+  }
+};
 
 class ChangePasswordView extends React.Component {
   constructor(props) {
@@ -31,21 +48,33 @@ class ChangePasswordView extends React.Component {
 
   render() {
     return (
-      <div>
-        <h4>Change your password</h4>
+      <form onSubmit={this.updatePassword.bind(this)} className="col-xs-12 col-md-8 col-lg-6">
+        <h2 style={styles.headline}>Change your password</h2>
+
         {NotificationBox(this.props.error, 'error')}
-        <form onSubmit={this.updatePassword.bind(this)}>
-          <label onChange={this.handleInputChange} htmlFor="password">
-            Password
-            <input type="password" name="password"></input>
-          </label>
-          <label onChange={this.handleInputChange} htmlFor="password_confirmation">
-            Confirm password
-            <input type="password" name="password_confirmation"></input>
-          </label>
-          <input type="submit" className="btn" value="update password" />
-        </form>
-      </div>);
+
+        <TextField
+          floatingLabelText="Password"
+          type="password"
+          id="password"
+          name="password"
+          required="true"
+          fullWidth={true}
+          onChange={this.handleInputChange}
+        />
+
+        <TextField
+          floatingLabelText="Confirm password"
+          type="password_confirmation"
+          id="password_confirmation"
+          name="password_confirmation"
+          required="true"
+          fullWidth={true}
+          onChange={this.handleInputChange}
+        />
+
+        <RaisedButton label="Update password" primary={true} type="submit" style={styles.sendButton} />
+    </form>);
   }
 }
 
