9d4b4796d948106a489c37d590df3a2ce4ce315b
Paweł Kowalski <pavelloz@gmail.com>
2016-06-12 18:05:41 +0200

    Improve code style

--- src/app/reducers/settings.js
+++ src/app/reducers/settings.js
@@ -4,7 +4,7 @@ const positions = JSON.parse(ls.get('PAGE_POSITIONS')) || {};
 
 const initialState = {
   languages: [
-    { code: 'pl-PL', key: 'pl', name: 'Polish'},
+    { code: 'pl-PL', key: 'pl', name: 'Polish' },
     { code: 'en-GB', key: 'en', name: 'English' },
     { code: 'fr-FR', key: 'fr', name: 'French' },
     { code: 'pt-BR', key: 'pt', name: 'Portuguese' },
@@ -21,44 +21,43 @@ const initialState = {
 
 };
 
-function language(state = initialState.language, action){
-
-  switch(action.type) {
+function language(state = initialState.language, action) {
+  switch (action.type) {
   case 'CHANGE_LANGUAGE':
-    return Object.assign({}, state, {[action.langType] : action.key});
+    return Object.assign({}, state, { [action.langType]: action.key });
 
   default:
     return state;
   }
-};
+}
 
-function articlePositions(state = positions, action){
-  switch(action.type){
+function articlePositions(state = positions, action) {
+  switch (action.type) {
   case 'PAGE_SCROLLED':
-    return Object.assign({}, state, {[action.payload.pageId] : action.payload.position});
+    return Object.assign({}, state, { [action.payload.pageId]: action.payload.position });
 
   default:
     return state;
   }
-};
+}
 
-function navBarVisibility(state = initialState.navOpen, action){
-  switch(action.type){
+function navBarVisibility(state = initialState.navOpen, action) {
+  switch (action.type) {
   case 'ARTICLE_LOADED':
     return initialState.navOpen;
 
   case 'NAV_CLOSED':
-    return Object.assign({}, state.navOpen, {[action.side] : false});
+    return Object.assign({}, state.navOpen, { [action.side]: false });
 
   case '@@router/LOCATION_CHANGE':
-    return Object.assign({}, state.navOpen, {left : false});
+    return Object.assign({}, state.navOpen, { left: false });
 
   case 'TEXT_SELECTED':
-    return Object.assign({}, state.navOpen, {right : !!action.text});
+    return Object.assign({}, state.navOpen, { right: !!action.text });
 
   case
     'NAV_OPENED':
-    return Object.assign({}, state.navOpen, {[action.side] : true});
+    return Object.assign({}, state.navOpen, { [action.side]: true });
 
   default:
     return state;
@@ -66,32 +65,32 @@ function navBarVisibility(state = initialState.navOpen, action){
 }
 
 export default function settings(state = initialState, action) {
-  switch (action.type){
+  switch (action.type) {
 
   case 'PROCESS_STARTED':
-    return Object.assign({}, state, {processesCounter: state.processesCounter + 1});
+    return Object.assign({}, state, { processesCounter: state.processesCounter + 1 });
 
   case 'PROCESS_FINISHED':
-    return Object.assign({}, state, {processesCounter: state.processesCounter - 1});
+    return Object.assign({}, state, { processesCounter: state.processesCounter - 1 });
 
   case 'CHANGE_LANGUAGE':
-    return Object.assign({}, state, {language: language(state.language, action)});
+    return Object.assign({}, state, { language: language(state.language, action) });
 
   case 'ARTICLE_LOADED':
   case 'NAV_CLOSED':
   case 'TEXT_SELECTED':
   case 'NAV_OPENED':
   case '@@router/LOCATION_CHANGE':
-    return Object.assign({}, state, {navOpen: navBarVisibility(state.navOpen, action)});
+    return Object.assign({}, state, { navOpen: navBarVisibility(state.navOpen, action) });
 
   case 'PAGE_SCROLLED':
     return Object.assign(
       {},
       state,
-      {articlePositions: articlePositions(state.articlePositions, action)}
+      { articlePositions: articlePositions(state.articlePositions, action) }
     );
 
   default:
     return state;
   }
-};
+}
