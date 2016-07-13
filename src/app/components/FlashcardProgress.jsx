3f6714460904f36b86c3e9943b394a7e52461362
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2016-07-13 23:14:21 +0200

    Flashcards: remove stats per basket

--- src/app/components/FlashcardProgress.jsx
+++ src/app/components/FlashcardProgress.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import _ from 'lodash';
 
 const FlashcardProgress = (props) => {
-  const stats = Object.assign({ 0: 0, 1: 0, 2: 0 }, _.countBy(props.items, 'group'));
   return (
     <div className="row center-xs">
       <div className="col-xs-8">
-        <p>{stats[0]} - {stats[1]} - {stats[2]}</p>
         <p>{props.items.length} words in lesson</p>
       </div>
     </div>
