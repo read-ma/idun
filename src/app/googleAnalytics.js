265e822ddde848fa9940b287c113b7d3e9fc264f
dariusz gorzeba <dgorzeba@gmail.com>
2016-07-20 14:01:30 +0200

    GA debug mode ON

--- src/app/googleAnalytics.js
+++ src/app/googleAnalytics.js
@@ -3,7 +3,7 @@ import { hashHistory } from 'react-router';
 import ls from './localStore';
 
 export default () => {
-  ReactGA.initialize('UA-79667263-1');
+  ReactGA.initialize('UA-79667263-1', {debug: true});
   ReactGA.set({ userId: ls.get('CURRENT_USER_EMAIL') });
   hashHistory.listen(location => ReactGA.pageview(location.pathname));
-}
+};
