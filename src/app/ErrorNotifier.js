0dcb83d24872cca5a5ea0c59767edd716d433bc0
Paweł Kowalski <pavelloz@gmail.com>
2016-07-22 21:41:02 +0200

    Remove unused eslint config --skip-ci

--- src/app/ErrorNotifier.js
+++ src/app/ErrorNotifier.js
@@ -1,17 +1,17 @@
-import Airbrake from 'airbrake-js'
+import Airbrake from 'airbrake-js';
 
 class ErrorNotifier {
   static setup() {
     let env = process.env.NODE_ENV;
 
-    if (env == 'production') {
-      var airbrake = new Airbrake({
+    if (env === 'production') {
+      let airbrake = new Airbrake({
         projectId: '8a87ad8854a89f8e33a221b692d5c3b4',
         projectKey: '8a87ad8854a89f8e33a221b692d5c3b4',
         reporter: 'xhr',
         host: 'http://errbit.logdock.com'
       });
-     window.onerror = airbrake.onerror;
+      window.onerror = airbrake.onerror;
     }
   }
 }
