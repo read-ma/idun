4a37b21f62fca857ac4ee99d7e3ee27912e6414b
Paweł Kowalski <pavelloz@gmail.com>
2016-07-31 12:28:47 +0200

    Add eslint config for unit tests

--- __tests__/local-storage-test.js
+++ __tests__/local-storage-test.js
@@ -3,23 +3,21 @@ jest.unmock('../src/app/localStore.js');
 import localStore from '../src/app/localStore.js';
 
 describe('LocalStorage', () => {
-
-  it ('handles strings ', () => {
+  it('handles strings ', () => {
     localStore.set('STRING', 'some text');
 
     expect(localStore.get('STRING')).toEqual('some text');
   });
 
-  it ('handles booleans ', () => {
+  it('handles booleans ', () => {
     localStore.set('BOOLEAN', false);
 
     expect(localStore.get('BOOLEAN', 'bool')).toEqual(false);
   });
 
-  it ('handles true booleans ', () => {
+  it('handles true booleans ', () => {
     localStore.set('BOOLEAN', true);
 
     expect(localStore.get('BOOLEAN', 'bool')).toEqual(true);
   });
-
 });
